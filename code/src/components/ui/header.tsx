9fab9611c7e80bae145010b41a57b999767571a5
Nils <nilsvandepitte9@gmail.com>
2025-06-29 16:47:55 +0200

    Add Header component with title and subtitle props

---
+++ code/src/components/ui/header.tsx
@@ -0,0 +1,13 @@
+﻿interface TitleProps {
+  title: string
+  subTitle?: string
+}
+
+export function Header({ title, subTitle }: TitleProps) {
+  return (
+    <header className="ml-10">
+      <h1 className="font-medium">{title}</h1>
+      <h2 className="text-neutral-400">{subTitle}</h2>
+    </header>
+  )
+}
