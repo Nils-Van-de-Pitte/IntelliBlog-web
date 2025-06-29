1505a6f531a0a73c6cf471e34e13fbc423276e4a
Nils <nilsvandepitte9@gmail.com>
2025-06-29 17:15:56 +0200

    Update Posts page: include Post component with sample content, likes, and tags

--- code/src/app/posts/page.tsx
+++ code/src/app/posts/page.tsx
@@ -1,10 +1,18 @@
 ﻿import {Header} from "@/src/components/ui/header";
+import {Post} from "@/src/components/ui/post";
 
 export default function Page() {
   return (
-    <Header
-      title="Posts Overview"
-      subTitle="Quickly manage and update your content from one place"
-    />
+    <>
+      <Header
+        title="Posts Overview"
+        subTitle="Quickly manage and update your content from one place"
+      />
+      <Post title="Small Steps, Big Changes 🚀"
+            content="Every day is a chance to take one small step toward a bigger goal.
+            It doesn’t have to be perfect — it just has to move you forward. Keep showing up. Keep trying. That’s how growth happens🌱"
+            likes={5}
+            tags={["#motivation", "#progressnotperfection", "#keepgoing"]}/>
+    </>
   );
 }
