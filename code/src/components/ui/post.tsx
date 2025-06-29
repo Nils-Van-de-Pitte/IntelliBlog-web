d0c35f50d1a5c52e979eaffb40c655d104704bff
Nils <nilsvandepitte9@gmail.com>
2025-06-29 17:15:50 +0200

    Add Post component with title, content, tags, and likes display

---
+++ code/src/components/ui/post.tsx
@@ -0,0 +1,24 @@
+﻿interface PostProps {
+  title: string;
+  content: string;
+  tags: string[];
+  likes: number;
+}
+
+export function Post({title, content, tags, likes }: PostProps) {
+
+  return(
+    <div className="w-100 border-2 border-accent rounded-md p-4 ml-10 flex flex-col justify-between">
+      <h2 className="mb-5">{title}</h2>
+      <p className="mb-5">{content}</p>
+      <p className="text-blue-400">
+        {tags.map((tag) =>
+          <span key={tag} className="mr-1">
+            {tag}
+          </span>
+        )}
+      </p>
+      <p>{likes} likes</p>
+    </div>
+  );
+}
