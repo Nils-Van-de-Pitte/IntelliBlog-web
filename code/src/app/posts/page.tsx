6786334b0259fdac4299ea6f01ea68998f5946ca
Nils <nilsvandepitte9@gmail.com>
2025-06-29 17:28:03 +0200

    Update Posts page: add "Write a post" button and improve layout with flex alignment

--- code/src/app/posts/page.tsx
+++ code/src/app/posts/page.tsx
@@ -1,13 +1,26 @@
 ﻿import {Header} from "@/src/components/ui/header";
 import {Post} from "@/src/components/ui/post";
+import {Button} from "@/src/components/ui/button";
+import {JSX} from "react";
 
-export default function Page() {
+/**
+ * Renders the Posts Page.
+ *
+ * @return {JSX.Element} A JSX element containing a header with a title and subtitle,
+ *                       a button to create a post, and a sample post displaying
+ *                       content, likes, and tags.
+ */
+export default function Page(): JSX.Element {
   return (
     <>
-      <Header
-        title="Posts Overview"
-        subTitle="Quickly manage and update your content from one place"
-      />
+      <div className="flex justify-between">
+        <Header
+          title="Posts Overview"
+          subTitle="Quickly manage and update your content from one place"
+        />
+        <Button>Write a post</Button>
+      </div>
+
       <Post title="Small Steps, Big Changes 🚀"
             content="Every day is a chance to take one small step toward a bigger goal.
             It doesn’t have to be perfect — it just has to move you forward. Keep showing up. Keep trying. That’s how growth happens🌱"
