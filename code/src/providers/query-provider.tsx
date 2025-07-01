a048010b9b8390251f1ec7c125781df41e75d97b
Nils <nilsvandepitte9@gmail.com>
2025-07-01 14:27:44 +0200

    Add "use client" directive to QueryProvider for ensuring client-side rendering

--- code/src/providers/query-provider.tsx
+++ code/src/providers/query-provider.tsx
@@ -1,4 +1,6 @@
-﻿import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+﻿"use client";
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {JSX, ReactNode, useState} from 'react'
 
