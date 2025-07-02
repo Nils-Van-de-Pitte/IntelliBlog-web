6136c607cfd804c29847378b9d63b0a0be4ade87
Nils <nilsvandepitte9@gmail.com>
2025-07-02 12:04:20 +0200

    Add getPosts function to fetch posts from API using axios

---
+++ code/src/api/posts.ts
@@ -0,0 +1,10 @@
+﻿import get from "axios"
+
+export async function getPosts() {
+  try {
+    const response = await get("https://localhost:5000/api/posts");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
