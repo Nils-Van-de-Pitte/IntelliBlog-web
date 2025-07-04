52cc218dae7f9200323c37dc8ab30e43fd21c8a7
Nils <nilsvandepitte9@gmail.com>
2025-07-04 14:26:25 +0200

    Add Error component for displaying error messages with styled text

---
+++ code/src/components/ui/error.tsx
@@ -0,0 +1,12 @@
+﻿interface ErrorProps {
+  message: string;
+}
+
+
+export const Error = ({message}: ErrorProps) => {
+  return (
+    <p className="text-red-500 flex justify-center">
+      {message}
+    </p>
+  )
+}
